cb84fd548a69ef0afd01827f9c734e172c5a9d94
chrisjmccormick <chrisjmccormick@gmail.com>
2017-02-15 17:30:55 -0800

    Fixed globals(), added timing for sort

--- run_search.py
+++ run_search.py
@@ -10,18 +10,18 @@ import sys
 import time
 
 # Load the Wikipedia LSI vectors (if not already loaded)
-if not globals.has_key('index'):
+if not globals().has_key('index'):
     print 'Loading Wikipedia LSI index...'
     sys.stdout.flush()
     index = similarities.MatrixSimilarity.load('./data/lsi_index.mm')
 
 
 # Load the article titles. These have the format (pageid, article title)
-if not globals.has_key('titles_to_id'):
+if not (globals().has_key('id_to_titles') and globals().has_key('titles_to_id')):
     print 'Loading Wikipedia article titles'
     sys.stdout.flush()
     id_to_titles = utils.unpickle('./data/bow.mm.metadata.cpickle')
-    titles_to_id = utils.unpickle('/data/titles_to_id.pickle')
+    titles_to_id = utils.unpickle('./data/titles_to_id.pickle')
 
 query_title = 'Topic model'
 
@@ -45,7 +45,14 @@ elapsed = time.time() - t0
 print '    Similarity search took %.2f seconds' % elapsed
 
 # Sort in descending order.
+t0 = time.time()
 sims = sorted(enumerate(sims), key=lambda item: -item[1])
+
+elapsed = time.time() - t0
+
+print '    Sorting took %.2f' % elapsed
+
+print '\nResults:'
 
 # Display the top 10 results
 for i in range(0, 10):
